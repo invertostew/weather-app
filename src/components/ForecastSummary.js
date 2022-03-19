3d67863827c532ca2f4bb0f7149b80e895210848
Stuart Green <github.47a8z@aleeas.com>
2022-03-19 20:39:45 +0000

    feat: add weather icons and dates

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,16 +1,27 @@
 import PropTypes from "prop-types";
+import WeatherIcon from "react-icons-weather";
+import moment from "moment";
 
 import "../styles/ForecastSummary.css";
 
 function ForecastSummary({ date, icon, temperature, description }) {
   return (
     <article className="forecast-summary">
-      <div className="forecast-summary__date">{date}</div>
-      <div className="forecast-summary__icon">{icon}</div>
-      <div className="forecast-summary__temperature">
-        {temperature.max} &deg;C
+      <div className="forecast-summary__date">
+        <p>{moment(date).format("ddd Do MMM")}</p>
       </div>
-      <div className="forecast-summary__description">{description}</div>
+
+      <div className="forecast-summary__icon">
+        <WeatherIcon name="owm" iconId={icon} />
+      </div>
+
+      <div className="forecast-summary__temperature">
+        <p>{temperature.max} &deg;C</p>
+      </div>
+
+      <div className="forecast-summary__description">
+        <p>{description}</p>
+      </div>
     </article>
   );
 }

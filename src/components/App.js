a193613eeabd96fab1df365f5f5b633ae418aafe
Stuart Green <github.47a8z@aleeas.com>
2022-03-24 20:44:10 +0000

    feat: add state to select the forecast details

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
@@ -8,12 +9,16 @@ import "../styles/App.css";
 
 function App({ location, forecasts }) {
   const { city, country } = location;
+  const [selectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
 
   return (
     <main className="weather-app">
       <LocationDetails city={city} country={country} />
       <ForecastSummaries forecasts={forecasts} />
-      <ForecastDetails forecast={forecasts[0]} />
+      <ForecastDetails forecast={selectedForecast} />
     </main>
   );
 }

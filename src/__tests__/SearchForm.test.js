fad9e9dd581ce01bda7184831952aa1c90d8a698
Stuart Green <github.47a8z@aleeas.com>
2022-03-27 13:39:16 +0100

    test: search form component

---
+++ src/__tests__/SearchForm.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+
+import SearchForm from "../components/SearchForm";
+
+describe("SearchForm", () => {
+  const validProps = {
+    searchText: "Manchester",
+    setSearchText: jest.fn(),
+    handleCitySearch: jest.fn(),
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <SearchForm
+        searchText={validProps.searchText}
+        setSearchText={validProps.setSearchText}
+        handleCitySearch={validProps.handleCitySearch}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders the search button", () => {
+    const { getByText } = render(
+      <SearchForm
+        searchText={validProps.searchText}
+        setSearchText={validProps.setSearchText}
+        handleCitySearch={validProps.handleCitySearch}
+      />
+    );
+
+    expect(getByText("Search")).toBeInTheDocument();
+  });
+
+  it("renders the right placeholder text", () => {
+    render(<SearchForm />);
+
+    const inputNode = screen.getByPlaceholderText("Please enter a location");
+
+    expect(inputNode).toBeInTheDocument();
+  });
+});
